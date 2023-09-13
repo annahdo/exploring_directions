5ecea422e3afe50cbee2a9c325727289019fcb4c
Ann-Kathrin Dombrowski <annah.katharina@gmail.com>
2023-09-13 10:58:49 +0000

    user input to beginning, imports

--- modules/utils.py
+++ modules/utils.py
@@ -1,6 +1,7 @@
 from matplotlib import pyplot as plt
 import seaborn as sns
 import pandas as pd
+import numpy as np
 
 # plot functions
 def plot_lines(data_dict, y_name, save_path=None, method_names=None):
@@ -55,3 +56,34 @@ def plot_bars(data_dict, y_name, save_path=None):
     if save_path:
         plt.savefig(save_path)
     plt.show()
+
+def load_util_data(data_dir, split="test"):
+    data = pd.read_csv(data_dir + f'util_{split}.csv', header=None)
+    sentences = []
+    for d in data.iloc:
+        sentences.append([d[0], d[1]])
+    return np.array(sentences)
+
+
+def mix_util_pairs(data, random_seed=0):
+    # per default always the first sentence is the sentence with higher utility
+    # this function swaps the sentences with probability 0.5
+    rng = np.random.default_rng(random_seed)
+    m = len(data)
+    labels = rng.integers(0, 2, size=m)
+    data = data[np.arange(m), [1-labels, labels]].T
+    return data, labels
+
+def find_two_sentences(data, split_str1=".", split_str2=",", larger_than1=2, larger_than2=1):
+    idxs = []
+    sentences = []
+
+    for i, d in enumerate(data):
+        if len(d.split(split_str1)) > larger_than1:
+            idxs.append(i)
+            sentences.append(d.split(split_str1)[0] + f"{split_str1}")
+        elif split_str2:
+            if len(d.split(split_str2)) > larger_than2:
+                sentences.append(d.split(split_str2)[0]+f"{split_str2}")
+                idxs.append(i)
+    return idxs, sentences
